986f1dee6f4aa5cfb09b7015d8c492a5ea4da066
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2026-05-16 20:09:38 +0700

    change logo import in head

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Kos Rumah Bata | Hunian Nyaman, Booking Online</title>
-        <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
+        <link rel="icon" type="image/png" href="/logo.png">
         <meta name="description" content="Kos modern di Yogyakarta. Eksplorasi kamar, ajukan sewa, dan bayar online dengan praktis di Kos Rumah Bata.">
         <meta name="author" content="Kos Rumah Bata">
         <meta property="og:type" content="website">
