e91ed211d9f48d2e3d1ac12521ea11c6885507d9
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2026-05-16 20:31:00 +0700

    finish dynamic detail kamar

--- app/Http/Controllers/KamarController.php
+++ app/Http/Controllers/KamarController.php
@@ -11,10 +11,8 @@ class KamarController extends Controller
      */
     public function index()
     {
-        // Mengambil data kamar terbaru berdasarkan ID (kamar baru berada paling atas)
         $kamars = Kamar::orderBy('id', 'desc')->get();
 
-        // Mengembalikan view kamar di folder resources/views/kamar.blade.php
         return view('kamar', compact('kamars'));
     }
 
@@ -23,10 +21,8 @@ class KamarController extends Controller
      */
     public function show($id)
     {
-        // Mencari data kamar berdasarkan ID, jika tidak ditemukan langsung mengembalikan error 404
         $kamar = Kamar::findOrFail($id);
 
-        // Mengembalikan view detail kamar (misal: resources/views/detail-kamar.blade.php)
         return view('detail-kamar', compact('kamar'));
     }
 }
